61faa66b8f656835ed43d8cf59895c949f3d8b63
Abu Walad <abuwalad.mt@gmail.com>
2025-06-11 16:08:28 +0700

    upload

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from pathlib import Path
+
+app = FastAPI()
+
+BASE_DIR = Path(__file__).resolve().parent
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
